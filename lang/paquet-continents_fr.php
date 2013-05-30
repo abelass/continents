8547d26e35ada781688fbc23295d5efe2baea622
Rainer Müller <rainer@websimple.be>
2013-05-30 16:18:51 +0200

    inport depuis la fabrique

---
+++ lang/paquet-continents_fr.php
@@ -0,0 +1,14 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'continents_description' => 'Lier les pays au continents',
+	'continents_nom' => 'Continents',
+	'continents_slogan' => 'Lier les pays au continents',
+);
+
+?>
